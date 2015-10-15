de7a4a3406b71206ea533c1a5bcb77cee220fd97
wolfogre <wolfogre@qq.com>
2015-10-15 22:05:59 +0800

    新解决方案IVCurvometerTool

---
+++ SJX/IVCurvometerTool/IVCurvometerTestTool/MainForm.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace IVCurvometerTestTool
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.comboBox4 = new System.Windows.Forms.ComboBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.comboBox5 = new System.Windows.Forms.ComboBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			this.groupBox4 = new System.Windows.Forms.GroupBox();
+			this.groupBox5 = new System.Windows.Forms.GroupBox();
+			this.groupBox6 = new System.Windows.Forms.GroupBox();
+			this.groupBox7 = new System.Windows.Forms.GroupBox();
+			this.comboBox6 = new System.Windows.Forms.ComboBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.groupBox1.SuspendLayout();
+			this.groupBox7.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.comboBox5);
+			this.groupBox1.Controls.Add(this.label5);
+			this.groupBox1.Controls.Add(this.comboBox4);
+			this.groupBox1.Controls.Add(this.label4);
+			this.groupBox1.Controls.Add(this.comboBox3);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.comboBox2);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.comboBox1);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Location = new System.Drawing.Point(12, 12);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(125, 192);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "通讯设置";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(6, 31);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 12);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "串口号";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "COM1",
+            "无校检"});
+			this.comboBox1.Location = new System.Drawing.Point(53, 28);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(66, 20);
+			this.comboBox1.TabIndex = 1;
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+            "COM1",
+            "无校检"});
+			this.comboBox2.Location = new System.Drawing.Point(53, 54);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(66, 20);
+			this.comboBox2.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(6, 57);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(41, 12);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "串口号";
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Items.AddRange(new object[] {
+            "COM1",
+            "无校检"});
+			this.comboBox3.Location = new System.Drawing.Point(53, 80);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(66, 20);
+			this.comboBox3.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(6, 83);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(41, 12);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "串口号";
+			// 
+			// comboBox4
+			// 
+			this.comboBox4.FormattingEnabled = true;
+			this.comboBox4.Items.AddRange(new object[] {
+            "COM1",
+            "无校检"});
+			this.comboBox4.Location = new System.Drawing.Point(53, 106);
+			this.comboBox4.Name = "comboBox4";
+			this.comboBox4.Size = new System.Drawing.Size(66, 20);
+			this.comboBox4.TabIndex = 7;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(6, 109);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(41, 12);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "串口号";
+			// 
+			// comboBox5
+			// 
+			this.comboBox5.FormattingEnabled = true;
+			this.comboBox5.Items.AddRange(new object[] {
+            "COM1",
+            "无校检"});
+			this.comboBox5.Location = new System.Drawing.Point(53, 132);
+			this.comboBox5.Name = "comboBox5";
+			this.comboBox5.Size = new System.Drawing.Size(66, 20);
+			this.comboBox5.TabIndex = 9;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(6, 135);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(41, 12);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "串口号";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(23, 158);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 10;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Location = new System.Drawing.Point(143, 12);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(305, 95);
+			this.groupBox2.TabIndex = 1;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "groupBox2";
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Location = new System.Drawing.Point(143, 109);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(305, 95);
+			this.groupBox3.TabIndex = 2;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "groupBox3";
+			// 
+			// groupBox4
+			// 
+			this.groupBox4.Location = new System.Drawing.Point(12, 210);
+			this.groupBox4.Name = "groupBox4";
+			this.groupBox4.Size = new System.Drawing.Size(125, 217);
+			this.groupBox4.TabIndex = 3;
+			this.groupBox4.TabStop = false;
+			this.groupBox4.Text = "groupBox4";
+			// 
+			// groupBox5
+			// 
+			this.groupBox5.Location = new System.Drawing.Point(454, 12);
+			this.groupBox5.Name = "groupBox5";
+			this.groupBox5.Size = new System.Drawing.Size(305, 95);
+			this.groupBox5.TabIndex = 4;
+			this.groupBox5.TabStop = false;
+			this.groupBox5.Text = "groupBox5";
+			// 
+			// groupBox6
+			// 
+			this.groupBox6.Location = new System.Drawing.Point(454, 109);
+			this.groupBox6.Name = "groupBox6";
+			this.groupBox6.Size = new System.Drawing.Size(305, 95);
+			this.groupBox6.TabIndex = 5;
+			this.groupBox6.TabStop = false;
+			this.groupBox6.Text = "groupBox6";
+			// 
+			// groupBox7
+			// 
+			this.groupBox7.Controls.Add(this.panel1);
+			this.groupBox7.Controls.Add(this.textBox1);
+			this.groupBox7.Controls.Add(this.label7);
+			this.groupBox7.Controls.Add(this.comboBox6);
+			this.groupBox7.Controls.Add(this.label6);
+			this.groupBox7.Location = new System.Drawing.Point(143, 210);
+			this.groupBox7.Name = "groupBox7";
+			this.groupBox7.Size = new System.Drawing.Size(616, 217);
+			this.groupBox7.TabIndex = 6;
+			this.groupBox7.TabStop = false;
+			this.groupBox7.Text = "groupBox7";
+			// 
+			// comboBox6
+			// 
+			this.comboBox6.FormattingEnabled = true;
+			this.comboBox6.Items.AddRange(new object[] {
+            "COM1",
+            "无校检",
+            "电源电压系数测量"});
+			this.comboBox6.Location = new System.Drawing.Point(252, 14);
+			this.comboBox6.Name = "comboBox6";
+			this.comboBox6.Size = new System.Drawing.Size(122, 20);
+			this.comboBox6.TabIndex = 11;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(205, 17);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(41, 12);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "命令码";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(18, 17);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(53, 12);
+			this.label7.TabIndex = 12;
+			this.label7.Text = "测试仪ID";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(77, 14);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(122, 21);
+			this.textBox1.TabIndex = 13;
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(6, 41);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(604, 170);
+			this.panel1.TabIndex = 14;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(767, 435);
+			this.Controls.Add(this.groupBox7);
+			this.Controls.Add(this.groupBox6);
+			this.Controls.Add(this.groupBox5);
+			this.Controls.Add(this.groupBox4);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "MainForm";
+			this.Text = "IV曲线仪通讯测试";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox7.ResumeLayout(false);
+			this.groupBox7.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ComboBox comboBox5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.ComboBox comboBox4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.GroupBox groupBox3;
+		private System.Windows.Forms.GroupBox groupBox4;
+		private System.Windows.Forms.GroupBox groupBox5;
+		private System.Windows.Forms.GroupBox groupBox6;
+		private System.Windows.Forms.GroupBox groupBox7;
+		private System.Windows.Forms.ComboBox comboBox6;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
